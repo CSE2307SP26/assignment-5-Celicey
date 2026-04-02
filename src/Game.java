982a626d77cefdcc63b239823708831fc4427a84
Celicey <celiceyavalon@gmail.com>
2026-04-02 11:16:33 -0500

    New Class

--- src/Game.java
+++ src/Game.java
@@ -1,150 +1,130 @@
+//*
+// To Walk you through what I have been doing in steps, the first thing I did
+// was try to determine what the variables were, renaming them as I went */
+
+
+
+
+
+
+
+
+
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
-
 	public static void main(String[] args) {
-		double lv = 0.005;
-		double uv = 0.01;
-		int b = 3;
-		double r = 0.025;
-		int s = 0;
-		int hs = 0;
-		double px = 0.5;
-		double py = 0.5;
-		double ps = 0.01;
-		double[] bx = new double[b];
-		double[] bY = new double[b];
-		double[] bxv = new double[b];
-		double[] bYv = new double[b];
+		Balls redBalls = new Balls();
 		
-		for(int i = 0; i < b; i++) {
-			bx[i] = Math.random();
-			bY[i] = Math.random();
-			bxv[i] = Math.random() * (uv - lv) + lv;
-			bYv[i] = Math.random() * (uv - lv) + lv;
-		}
+		int score = 0;
+		int highScore = 0;
+		double positionX = 0.5;
+		double positionY = 0.5;
+		double playerSpeed = 0.01;
+
 		
+		redBalls.ballSpawn();
+
+
 		StdDraw.enableDoubleBuffering();
 		
-		long st = System.currentTimeMillis();
-		long dt = System.currentTimeMillis();
+		long scoretime = System.currentTimeMillis();
+		long difficultyIncrementTime = System.currentTimeMillis();
 		
 		while (true) {
 			
 			StdDraw.clear();
-			boolean c = false;
-			for(int i = 0; i < b; i++) {
+			boolean collision = false;
+			for(int i = 0; i < redBalls.ballCount; i++) {
 				
 			
-				bx[i] = bx[i] + bxv[i];
-				bY[i] = bY[i] + bYv[i];
-				if(bx[i] + r > 1 || bx[i] - r < 0) { 
-					bxv[i] = -bxv[i];
+				redBalls.ballPositionX[i] = redBalls.ballPositionX[i] + redBalls.ballVelocityX[i];
+				redBalls.ballPositionY[i] = redBalls.ballPositionY[i] + redBalls.ballVelocityY[i];
+				if(redBalls.ballPositionX[i] + redBalls.ballRadius > 1 || redBalls.ballPositionX[i] - redBalls.ballRadius < 0) { 
+					redBalls.ballVelocityX[i] = -redBalls.ballVelocityX[i];
 				}
-				if(bY[i] + r > 1 || bY[i] - r < 0) { 
-					bYv[i] = -bYv[i];
+				if(redBalls.ballPositionY[i] + redBalls.ballRadius > 1 || redBalls.ballPositionY[i] - redBalls.ballRadius < 0) { 
+					redBalls.ballVelocityY[i] = -redBalls.ballVelocityY[i];
 				}
-				for(int j = 0; j < b; j++) {
+				for(int j = 0; j < redBalls.ballCount; j++) {
 					if(i != j) {
-						double d = Math.sqrt(Math.pow(bx[i] - bx[j], 2) + Math.pow(bY[i] - bY[j], 2));
-						if(d < 2 * r) {
-							bxv[i] = -bxv[i];
-							bYv[i] = -bYv[i];
+						double d = Math.sqrt(Math.pow(redBalls.ballPositionX[i] - redBalls.ballPositionX[j], 2) + Math.pow(redBalls.ballPositionY[i] - redBalls.ballPositionY[j], 2));
+						if(d < 2 * redBalls.ballRadius) {
+							redBalls.ballVelocityX[i] = -redBalls.ballVelocityX[i];
+							redBalls.ballVelocityY[i] = -redBalls.ballVelocityY[i];
 						}
 					}
 				}
 				
-				double d = Math.sqrt(Math.pow(bx[i] - px, 2) + Math.pow(bY[i] - py, 2));
-				if(d < 2 * r) {
-					c = true;
+				double d = Math.sqrt(Math.pow(redBalls.ballPositionX[i] - positionX, 2) + Math.pow(redBalls.ballPositionY[i] - positionY, 2));
+				if(d < 2 * redBalls.ballRadius) {
+					collision = true;
 				}
 			}
 			
-			if(c) {
-				b = 3;
-				for(int i = 0; i < b; i++) {
-					bx[i] = Math.random();
-					bY[i] = Math.random();
-					bxv[i] = Math.random() * (uv - lv) + lv;
-					bYv[i] = Math.random() * (uv - lv) + lv;
-					s = 0;
-					st = System.currentTimeMillis();
-					dt = System.currentTimeMillis();
-					px = 0.5;
-					py = 0.5;
-				}
+			if(collision) {
+				redBalls.collision();
+
+				score = 0;
+				scoretime = System.currentTimeMillis();
+				difficultyIncrementTime = System.currentTimeMillis();
+				positionX = 0.5;
+				positionY = 0.5;
+				}			
 				
 				
-			}
+			
 			
 			if(StdDraw.isKeyPressed(KeyEvent.VK_W)) {
-				py = py + ps;
+				positionY = positionY + playerSpeed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_S)) {
-				py = py - ps;
+				positionY = positionY - playerSpeed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_A)) {
-				px = px - ps;
+				positionX = positionX - playerSpeed;
 			}
 			if(StdDraw.isKeyPressed(KeyEvent.VK_D)) {
-				px = px + ps;
+				positionX = positionX + playerSpeed;
 			}
 			
-			if(px > 1) {
-				px = 1;
+			if(positionX > 1) {
+				positionX = 1;
 			}
-			if(px < 0) {
-				px = 0;
+			if(positionX < 0) {
+				positionX = 0;
 			}
-			if(py > 1) {
-				py = 1;
+			if(positionY > 1) {
+				positionY = 1;
 			}
-			if(py < 0) {
-				py = 0;
+			if(positionY < 0) {
+				positionY = 0;
 			}
 			
 			long now = System.currentTimeMillis();
-			if(now > st + 1000) {
-				s++;
-				if(s > hs) {
-					hs = s;
+			if(now > scoretime + 1000) {
+				score++;
+				if(score > highScore) {
+					highScore = score;
 				}
-				st = now;
+				scoretime = now;
 			}
 			
-			if(now > dt + 10000) {
-				b++;
-				double[] ballXnew = new double[b];
-				double[] ballYnew = new double[b];
-				double[] ballXVnew = new double[b];
-				double[] ballYVnew = new double[b];
-				for(int i = 0; i < b - 1; i++) {
-					ballXnew[i] = bx[i];
-					ballYnew[i] = bY[i];
-					ballXVnew[i] = bxv[i];
-					ballYVnew[i] = bYv[i];
-				}
-				ballXnew[b-1] = Math.random();
-				ballYnew[b-1] = Math.random();
-				ballXVnew[b-1] = Math.random() * (uv - lv) + lv;
-				ballYVnew[b-1] = Math.random() * (uv - lv) + lv;
-				bx = ballXnew;
-				bY = ballYnew;
-				bxv = ballXVnew;
-				bYv = ballYVnew;
-				dt = now;
+			if(now > difficultyIncrementTime + 10000) {
+				redBalls.increaseDifficulty();
+				difficultyIncrementTime = now;
 			}
 			StdDraw.setPenColor(Color.red);
-			for(int i = 0; i < b; i++) {
-				StdDraw.filledCircle(bx[i], bY[i], r);
+			for(int i = 0; i < redBalls.ballCount; i++) {
+				StdDraw.filledCircle(redBalls.ballPositionX[i], redBalls.ballPositionY[i], redBalls.ballRadius);
 			}
 			
 			StdDraw.setPenColor(Color.black);
-			StdDraw.filledCircle(px, py, r);
-			StdDraw.text(0.5, 0.1, "Score: " + s + " High Score: " + hs);
+			StdDraw.filledCircle(positionX, positionY, redBalls.ballRadius);
+			StdDraw.text(0.5, 0.1, "Score: " + score + " High Score: " + highScore);
 			
 			StdDraw.show();
 			StdDraw.pause(10);
